6076adcb8dde7a1fa3170168f8c6f90c463c7337
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-24 08:33:08 -0300

    feat: spring security load user

---
+++ src/main/java/com/ypay/api/infra/security/SecurityService.java
@@ -0,0 +1,18 @@
+package com.ypay.api.infra.security;
+
+import com.ypay.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class SecurityService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Token inválido"));
+    }
+}
